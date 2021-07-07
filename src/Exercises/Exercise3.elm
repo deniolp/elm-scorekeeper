152ac910a046da8924e7d766e1f11b4ff674e85b
deniolp <deniolp@gmail.com>
2021-07-07 11:49:29 +0300

    Refactoring

--- src/Exercises/Exercise3.elm
+++ src/Exercises/Exercise3.elm
@@ -6,7 +6,9 @@ import Html exposing (Html)
 
 main : Html msg
 main =
-    Html.text (toString newCart)
+    List.map (getBonus 10 3 >> getBonus 5 1) cart
+        |> toString
+        |> Html.text
 
 
 type alias ItemInCart =
@@ -20,7 +22,7 @@ cart : List ItemInCart
 cart =
     [ { name = "Lemon", qty = 1, freeQty = 0 }
     , { name = "Apple", qty = 5, freeQty = 0 }
-    , { name = "Pear", qty = 10, freeQty = 0 }
+    , { name = "Pear", qty = 20, freeQty = 0 }
     ]
 
 
@@ -28,13 +30,8 @@ getBonus : Int -> Int -> ItemInCart -> ItemInCart
 getBonus qtyToGetBonus bonus item =
     if item.qty >= qtyToGetBonus && item.freeQty == 0 then
         { item
-            | freeQty = bonus
+            | freeQty = item.qty // qtyToGetBonus * bonus
         }
 
     else
         item
-
-
-newCart : List ItemInCart
-newCart =
-    List.map (getBonus 10 3 >> getBonus 5 1) cart
