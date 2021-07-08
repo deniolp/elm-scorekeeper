196e487cbaca23a16e9658844135c01ac532a3c5
deniolp <deniolp@gmail.com>
2021-07-08 18:27:26 +0300

    Adding model, update (one msg) and initial view

--- src/Scorekeeper/Main.elm
+++ src/Scorekeeper/Main.elm
@@ -1,8 +1,93 @@
 module Scorekeeper.Main exposing (..)
 
+import Browser
 import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
 
 
-main : Html msg
+
+-- model
+
+
+type alias Model =
+    { players : List Player
+    , name : String
+    , playerId : Maybe Int
+    , plays : List Play
+    }
+
+
+type alias Player =
+    { id : Int
+    , name : String
+    , points : Int
+    }
+
+
+type alias Play =
+    { id : Int
+    , playerId : Int
+    , name : String
+    , points : Int
+    }
+
+
+initModel : Model
+initModel =
+    Model [] "" Nothing []
+
+
+
+-- update
+
+
+type Msg
+    = Edit Player
+    | Score Player Int
+    | Input String
+    | Save
+    | Cancel
+    | DeletePlay Play
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Input name ->
+            Debug.log "Input updated model"
+                { model | name = name }
+
+        _ ->
+            model
+
+
+
+-- view
+
+
+view : Model -> Html Msg
+view model =
+    div [ class "scoreboard" ]
+        [ h1 [] [ text "Score Keeper" ]
+        , playerForm model
+        ]
+
+
+playerForm : Model -> Html Msg
+playerForm model =
+    Html.form [ onSubmit Save ]
+        [ input
+            [ type_ "text"
+            , placeholder "Add/Edit Player..."
+            , onInput Input
+            , value model.name
+            ]
+            []
+        , button [ type_ "submit" ] [ text "Save" ]
+        , button [ type_ "button", onClick Cancel ] [ text "Cancel" ]
+        ]
+
+
 main =
-    Html.text "Hello!"
+    Browser.sandbox { init = initModel, update = update, view = view }
