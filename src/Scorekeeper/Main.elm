e9293f6ae296e7d4bb77271e87beb101d1759a93
deniolp <deniolp@gmail.com>
2021-07-08 20:46:14 +0300

    Implementing Edit Msg

--- src/Scorekeeper/Main.elm
+++ src/Scorekeeper/Main.elm
@@ -72,6 +72,9 @@ update msg model =
         Score player points ->
             scorePoints model player points
 
+        Edit player ->
+            { model | name = player.name, playerId = Just player.id }
+
         _ ->
             model
 
